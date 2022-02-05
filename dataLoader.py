bed7f21f42cb557ea569886224bf81dfdcc10317
Amean Asad <amean.asad1999@gmail.com>
2022-02-05 13:25:22 -0500

    test

--- dataLoader.py
+++ dataLoader.py
@@ -8,42 +8,25 @@ Created on Wed Jan 26 12:01:11 2022
 
 from pathlib import Path
 import pickle
+import pandas as pd
+import numpy as np
 
-"""
-Loading Morphology file desrcibing structure of the neuron and coordinates
-of different branches.
-"""
-morphologyFilePath = Path("./files/morphology.pickle")
-morphologyFile = morphologyFilePath.open(mode="rb")
-morphologyDict = pickle.load(morphologyFile, encoding='latin1')
+dataFile = Path("data/simulations")
 
-segmentsLength = morphologyDict["all_segments_length"]
-segmentDistFromSoma = morphologyDict['all_sections_distance_from_soma']
-segmentsType = morphologyDict['all_segments_type']
-segmentsSectionDistFromSoma = morphologyDict['all_segments_section_distance_from_soma']
-segmentsSectionInd = morphologyDict['all_segments_section_index']
-segmentIndexWithinSectionIndex = morphologyDict[
-    'all_segments_segment_index_within_section_index']
+modelPaths = [p for p in list(dataFile.glob("*.p"))]
 
-basalSectionCoords = morphologyDict['all_basal_section_coords']
-basalSegmentCoords = morphologyDict['all_basal_segment_coords']
-apicalSectionCoords = morphologyDict['all_apical_section_coords']
-apicalSegmentCoords = morphologyDict['all_apical_segment_coords']
+currentPath = modelPaths[0]
+data = currentPath.open(mode="rb")
+data = pickle.load(data, encoding='latin1')
 
-segmentIdxToCoords = {}
-segmentIdxToSectionIdx = {}
+dataResults = data["Results"]["listOfSingleSimulationDicts"]
+dataParams = data["Params"]
 
-for idx, segment in enumerate(segmentsType[:5]):
+dataResult0 = dataResults[0]
 
-    currentSegmentIdx = segmentIndexWithinSectionIndex[idx]
-    if segment == "basal":
-        currentSectionIdx = segmentsSectionInd[idx]
-        segmentCoords = basalSegmentCoords[(currentSectionIdx, currentSegmentIdx)]
-        segmentIdxToCoords[idx] = segmentCoords
-        segmentIdxToSectionIdx[idx] = ("basal", currentSectionIdx)
+numDataPoints = dataParams["totalSimDurationInSec"]*1000
+numOfSimulations = len(dataResults)
 
-    if segment == "apical":
-        currentSectionIdx = segmentsSectionInd[idx] - len(basalSectionCoords)
-        segmentCoords = apicalSegmentCoords[(currentSectionIdx, currentSegmentIdx)]
-        segmentIdxToCoords[idx] = segmentCoords
-        segmentIdxToSectionIdx[idx] = ("apical", currentSectionIdx)
+
+
+X = np.zeros((639, numDataPoints, numOfSimulations))
