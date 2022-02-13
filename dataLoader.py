2fe7ed0735cabcf97f33e4c41b98ebcc25b31de1
Amean Asad <amean.asad1999@gmail.com>
2022-02-13 14:37:12 -0500

    Dataloading for model

--- dataLoader.py
+++ dataLoader.py
@@ -8,8 +8,8 @@ Created on Wed Jan 26 12:01:11 2022
 
 from pathlib import Path
 import pickle
-import pandas as pd
 import numpy as np
+from timeit import default_timer as timer
 
 dataFile = Path("data/simulations")
 
@@ -17,69 +17,134 @@ modelPaths = [p for p in list(dataFile.glob("*.p"))]
 
 
 def spikeDictToArray(spikeTimes, numOfSegments, numDataPoints):
-    
+
     spikeValuesMatrix = np.zeros((numOfSegments, numDataPoints))
     spikeTimeVals = spikeTimes.values()
-    
+
     for idx, spikeArray in enumerate(spikeTimeVals):
         for spikeTime in spikeArray:
             spikeValuesMatrix[idx, spikeTime] = 1.0
     return spikeValuesMatrix
-    
+
+
 def parseSimulationFile(filePath):
     """
     Parameters
     ----------
     filePath : Pathlib Path
         A filepath to a pickled simulation file generated.
-        The filepath must be a Pathlib path for the function to work. 
+        The filepath must be a Pathlib path for the function to work.
     Returns
     -------
-    Parsed 
-
+    Parsed
     """
+
+    print("Currently parsing file: {}".format(filePath.name))
+    startTime = timer()
+    
     data = filePath.open(mode="rb")
     data = pickle.load(data, encoding='latin1')
 
-    dataResults = data["Results"]["listOfSingleSimulationDicts"][:1]
+    dataResults = data["Results"]["listOfSingleSimulationDicts"][:2]
     dataParams = data["Params"]
 
     numDataPoints = dataParams["totalSimDurationInSec"]*1000
     numOfSimulations = len(dataResults)
     numOfSegments = len(dataParams["allSegmentsType"])
-    synapseCount = 639 * 2 # 639 Inhibitory + 639 Excitatory inputs
-    
+    synapseCount = 639 * 2  # 639 Inhibitory + 639 Excitatory inputs
 
     X = np.zeros((synapseCount, numDataPoints, numOfSimulations))
     spikeVals = np.zeros((numDataPoints, numOfSimulations))
     somaVoltages = np.zeros((numDataPoints, numOfSimulations))
     nexusVoltages = np.zeros((numDataPoints, numOfSimulations))
-    
-    dendriticVoltages  = np.zeros(
-        (numOfSegments, numDataPoints,numOfSimulations), dtype=np.float16)
 
-    
+    dendriticVoltages = np.zeros(
+        (numOfSegments, numDataPoints, numOfSimulations), dtype=np.float16)
+
     for idx, simulationResult in enumerate(dataResults):
-    
+
         inhibitorySpikes = spikeDictToArray(simulationResult["inhInputSpikeTimes"],
-                                    numOfSegments, numDataPoints)
+                                            numOfSegments, numDataPoints)
         excitatorySpikes = spikeDictToArray(simulationResult["exInputSpikeTimes"],
-                                    numOfSegments, numDataPoints)
-        
-        X[:,:, idx] = np.vstack((excitatorySpikes, inhibitorySpikes))
-        dendriticVoltages[:,:,idx] = simulationResult["dendriticVoltagesLowRes"]
-            
+                                            numOfSegments, numDataPoints)
+
+        X[:, :, idx] = np.vstack((excitatorySpikes, inhibitorySpikes))
+        dendriticVoltages[:, :, idx] = simulationResult["dendriticVoltagesLowRes"]
+
         spikeTimes = (simulationResult['outputSpikeTimes'].astype(float) - 0.5).astype(int)
         spikeVals[spikeTimes, idx] = 1.0
-        
-        somaVoltages[:,idx] = simulationResult["somaVoltageLowRes"]
-        nexusVoltages[:,idx] = simulationResult["nexusVoltageLowRes"]
-        
+
+        somaVoltages[:, idx] = simulationResult["somaVoltageLowRes"]
+        nexusVoltages[:, idx] = simulationResult["nexusVoltageLowRes"]
+
         somaVoltages[spikeTimes, idx] = 30
 
-    
-    return X, spikeVals, somaVoltages, nexusVoltages, dendriticVoltages, dataResults
-    
-    
-X, spike, soma, n, v, d = parseSimulationFile(modelPaths[0])
+    timeStamps = simulationResult["recordingTimeLowRes"]
 
+    endTime = timer() - startTime
+    print("Done, time elapsed: {} \n".format(round(endTime, 3)))
+
+    return X, spikeVals, somaVoltages, nexusVoltages, dendriticVoltages, timeStamps
+
+
+def parseSimulationFileForModel(filePath):
+    """
+    Parameters
+    ----------
+    filePath : Pathlib Path
+        A filepath to a pickled simulation file generated.
+        The filepath must be a Pathlib path for the function to work.
+    Returns
+    -------
+    Parsed
+    """
+
+    print("Currently parsing file: {}".format(filePath.name))
+    startTime = timer()
+    
+    data = filePath.open(mode="rb")
+    data = pickle.load(data, encoding='latin1')
+
+    dataResults = data["Results"]["listOfSingleSimulationDicts"][4:5]
+    dataParams = data["Params"]
+
+    numDataPoints = dataParams["totalSimDurationInSec"]*1000
+    numOfSimulations = len(dataResults)
+    numOfSegments = len(dataParams["allSegmentsType"])
+    synapseCount = 639 * 2  # 639 Inhibitory + 639 Excitatory inputs
+
+    X = np.zeros((synapseCount, numDataPoints*numOfSimulations))
+    spikeVals = np.zeros((numDataPoints*numOfSimulations))
+    somaVoltages = np.zeros((numDataPoints*numOfSimulations))
+    nexusVoltages = np.zeros((numDataPoints*numOfSimulations))
+
+    dendriticVoltages = np.zeros(
+        (numOfSegments, numDataPoints*numOfSimulations), dtype=np.float16)
+
+    for idx, simulationResult in enumerate(dataResults):
+
+        inhibitorySpikes = spikeDictToArray(simulationResult["inhInputSpikeTimes"],
+                                            numOfSegments, numDataPoints)
+        excitatorySpikes = spikeDictToArray(simulationResult["exInputSpikeTimes"],
+                                            numOfSegments, numDataPoints)
+        startIdx = numDataPoints*idx
+        endIdx = numDataPoints*(idx + 1)
+        X[:, startIdx: endIdx] = np.vstack((excitatorySpikes, inhibitorySpikes))
+        dendriticVoltages[:, startIdx:endIdx] = simulationResult["dendriticVoltagesLowRes"]
+
+        spikeTimes = (simulationResult['outputSpikeTimes'].astype(float) - 0.5).astype(int)
+        spikeVals[spikeTimes + startIdx] = 1.0
+
+        somaVoltages[startIdx:endIdx] = simulationResult["somaVoltageLowRes"]
+        nexusVoltages[startIdx:endIdx] = simulationResult["nexusVoltageLowRes"]
+
+        somaVoltages[spikeTimes + startIdx] = 30
+
+    # timeStamps = simulationResult["recordingTimeLowRes"]
+
+    endTime = timer() - startTime
+    print("Done, time elapsed: {} \n".format(round(endTime, 3)))
+
+    return X, somaVoltages, nexusVoltages, dendriticVoltages, spikeVals
+
+X, soma, nexus, dvt, spike =  parseSimulationFileForModel(modelPaths[0])
