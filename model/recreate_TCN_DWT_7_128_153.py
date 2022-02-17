cec91b90fd887a130c9bfc6f409a28b76acaa137
JuliaWier <16jw81@queensu.ca>
2022-02-17 10:17:21 -0500

    Update recreate_TCN_DWT_7_128_153.py
    Determined that batch layers need weight initialization and potentially 3 biases? Need to confirm if this replicates the paper results

--- model/recreate_TCN_DWT_7_128_153.py
+++ model/recreate_TCN_DWT_7_128_153.py
@@ -12,11 +12,17 @@ from pathlib import Path
 # Change the directory to fetch the .h5 files
 dataFile = Path("../data/Models/NMDA_TCN__DWT_7_128_153__model.h5")
 
+#Load the Keras model and show its architecture
+NN_model = tf.keras.models.load_model(dataFile)
+NN_model.summary()
+
 # Read the .h5 model that corresponds to the correct model architecture
 model = keras.models.load_model(dataFile)
 weights = model.get_weights()
 
 # Define the Pytorch Architecture (Inspired by the Keras Model)
+
+#Define Causal Convolution 1D (no built-in padding style like this exists in Pytorch)
 class CausalConv1d(torch.nn.Conv1d):
     def __init__(self,
                  in_channels,
@@ -44,6 +50,7 @@ class CausalConv1d(torch.nn.Conv1d):
             return result[:, :, :-self.__padding]
         return result
 
+# Old Causal CONV1D Definition - May need it in the future if above doesn't work
 # def CausalConv1d(in_channels, out_channels, kernel_size, stride=(1,), dilation=(1,), groups=1, bias=True):
 #     pad = (kernel_size - 1)*dilation
 #     return nn.Conv1d(in_channels, out_channels, kernel_size, stride=stride, padding=pad, dilation=dilation, groups=groups, bias=bias)
@@ -124,86 +131,111 @@ class Net(nn.Module):
 
         return spikes, soma, dendrites
 
+
 net = Net()
-print(net.conv1)
+
+
 # Compare the Keras and Pytorch Weights to verify correctness of defined Pytorch model
-print(f'keras weight {weights[0].shape} and Pytorch weight {net.conv1.weight.shape}')
-print(f'keras weight {weights[1].shape} and Pytorch weight {net.conv1.bias.shape}')
-print(f'keras weight {weights[6].shape} and Pytorch weight {net.conv2.weight.shape}')
-print(f'keras weight {weights[3].shape} and Pytorch weight {net.batch2.weight.shape}')
-print(f'keras weight {weights[4].shape} and Pytorch weight {net.conv3.weight.shape}')
-print(f'keras weight {weights[5].shape} and Pytorch weight {net.batch3.weight.shape}')
-print(f'keras weight {weights[6].shape} and Pytorch weight {net.conv4.weight.shape}')
-print(f'keras weight {weights[7].shape} and Pytorch weight {net.batch4.weight.shape}')
-print(f'keras weight {weights[8].shape} and Pytorch weight {net.conv5.weight.shape}')
-print(f'keras weight {weights[9].shape} and Pytorch weight {net.batch5.weight.shape}')
-print(f'keras weight {weights[10].shape} and Pytorch weight {net.conv6.weight.shape}')
-print(f'keras weight {weights[11].shape} and Pytorch weight {net.batch6.weight.shape}')
-print(f'keras weight {weights[12].shape} and Pytorch weight {net.conv7.weight.shape}')
-print(f'keras weight {weights[13].shape} and Pytorch weight {net.batch7.weight.shape}')
+# print(f'keras weight {weights[0].shape} and Pytorch weight {net.conv1.weight.shape}')
+# print(f'keras weight {weights[1].shape} and Pytorch weight {net.conv1.bias.shape}')
+# print(f'keras weight {weights[6].shape} and Pytorch weight {net.conv2.weight.shape}')
+# print(f'keras weight {weights[3].shape} and Pytorch weight {net.batch2.weight.shape}')
+# print(f'keras weight {weights[4].shape} and Pytorch weight {net.conv3.weight.shape}')
+# print(f'keras weight {weights[5].shape} and Pytorch weight {net.batch3.weight.shape}')
+# print(f'keras weight {weights[6].shape} and Pytorch weight {net.conv4.weight.shape}')
+# print(f'keras weight {weights[7].shape} and Pytorch weight {net.batch4.weight.shape}')
+# print(f'keras weight {weights[8].shape} and Pytorch weight {net.conv5.weight.shape}')
+# print(f'keras weight {weights[9].shape} and Pytorch weight {net.batch5.weight.shape}')
+# print(f'keras weight {weights[10].shape} and Pytorch weight {net.conv6.weight.shape}')
+# print(f'keras weight {weights[11].shape} and Pytorch weight {net.batch6.weight.shape}')
+# print(f'keras weight {weights[12].shape} and Pytorch weight {net.conv7.weight.shape}')
+# print(f'keras weight {weights[13].shape} and Pytorch weight {net.batch7.weight.shape}')
 
 
 # Load the weights into Pytorch Model
 # Transpose the weights, but not the biases (Keras vs Pytorch syntax)
+#Convolutional Layer 1
 net.conv1.weight.data = torch.from_numpy(np.transpose(weights[0]))
-net.conv1[0].bias.data = torch.from_numpy(weights[1])
-net.conv1[1].bias.data = torch.from_numpy(weights[2])
-net.conv1[2].bias.data = torch.from_numpy(weights[3])
-net.conv1[3].bias.data = torch.from_numpy(weights[4])
-net.conv1[4].bias.data = torch.from_numpy(weights[5])
+net.conv1.bias.data = torch.from_numpy(weights[1])
+
+#Batch Layer 1
+net.batch1.weight.data = torch.from_numpy(weights[2])
+net.batch1.bias[0].data = torch.from_numpy(weights[3])
+net.batch1.bias[1].data = torch.from_numpy(weights[4])
+net.batch1.bias[2].data = torch.from_numpy(weights[5])
 
 
+#Convolutional Layer 2
 net.conv2.weight.data = torch.from_numpy(np.transpose(weights[6]))
-# net.conv2[0].bias.data = torch.from_numpy(weights[7])
-# net.conv2[1].bias.data = torch.from_numpy(weights[8])
-# net.conv1[2].bias.data = torch.from_numpy(weights[9])
-# net.conv1[3].bias.data = torch.from_numpy(weights[10])
-# net.conv1[4].bias.data = torch.from_numpy(weights[11])
+net.conv2.bias.data = torch.from_numpy(weights[7])
 
+#Batch Layer 2
+net.batch2.weight.data = torch.from_numpy(weights[8])
+net.batch2.bias[0].data = torch.from_numpy(weights[9])
+net.batch2.bias[1].data = torch.from_numpy(weights[10])
+net.batch2.bias[2].data = torch.from_numpy(weights[11])
+
+
+#Convolutional Layer 3
 net.conv3.weight.data = torch.from_numpy(np.transpose(weights[12]))
-# net.conv1[0].bias.data = torch.from_numpy(weights[13])
-# net.conv1[1].bias.data = torch.from_numpy(weights[14])
-# net.conv1[2].bias.data = torch.from_numpy(weights[15])
-# net.conv1[3].bias.data = torch.from_numpy(weights[16])
-# net.conv1[4].bias.data = torch.from_numpy(weights[17])
+net.conv3.bias.data = torch.from_numpy(weights[13])
 
+#Batch Layer 3
+net.batch3.weight.data= torch.from_numpy(weights[14])
+net.batch3.bias[0].data = torch.from_numpy(weights[15])
+net.batch3.bias[1].data = torch.from_numpy(weights[16])
+net.batch3.bias[2].data = torch.from_numpy(weights[17])
+
+#Convolutional Layer 4
 net.conv4.weight.data = torch.from_numpy(np.transpose(weights[18]))
-# net.conv1[0].bias.data = torch.from_numpy(weights[19])
-# net.conv1[1].bias.data = torch.from_numpy(weights[20])
-# net.conv1[2].bias.data = torch.from_numpy(weights[21])
-# net.conv1[3].bias.data = torch.from_numpy(weights[22])
-# net.conv1[4].bias.data = torch.from_numpy(weights[23])
+net.conv1.bias.data = torch.from_numpy(weights[19])
 
+#Batch Layer 4
+net.batch4.weight.data = torch.from_numpy(weights[20])
+net.batch4.bias[0].data = torch.from_numpy(weights[21])
+net.batch4.bias[1].data = torch.from_numpy(weights[22])
+net.batch4.bias[2].data = torch.from_numpy(weights[23])
+
+
+#Convolutional Layer 5
 net.conv5.weight.data = torch.from_numpy(np.transpose(weights[24]))
-# net.conv1[0].bias.data = torch.from_numpy(weights[25])
-# net.conv1[1].bias.data = torch.from_numpy(weights[26])
-# net.conv1[2].bias.data = torch.from_numpy(weights[27])
-# net.conv1[3].bias.data = torch.from_numpy(weights[28])
-# net.conv1[4].bias.data = torch.from_numpy(weights[29])
+net.conv5.bias.data = torch.from_numpy(weights[25])
 
+#Batch Layer 5
+net.batch5.weight.data = torch.from_numpy(weights[26])
+net.batch5.bias[0].data = torch.from_numpy(weights[27])
+net.batch5.bias[1].data = torch.from_numpy(weights[28])
+net.batch5.bias[2].data = torch.from_numpy(weights[29])
+
+#Convolutional Layer 6
 net.conv6.weight.data = torch.from_numpy(np.transpose(weights[30]))
-# net.conv1[0].bias.data = torch.from_numpy(weights[31])
-# net.conv1[1].bias.data = torch.from_numpy(weights[32])
-# net.conv1[2].bias.data = torch.from_numpy(weights[33])
-# net.conv1[3].bias.data = torch.from_numpy(weights[34])
-# net.conv1[4].bias.data = torch.from_numpy(weights[35])
+net.conv6.bias.data = torch.from_numpy(weights[31])
 
+#Batch Layer 6
+net.batch6.weight.data = torch.from_numpy(weights[32])
+net.batch6.bias[0].data = torch.from_numpy(weights[33])
+net.batch6.bias[1].data = torch.from_numpy(weights[34])
+net.batch6.bias[2].data = torch.from_numpy(weights[35])
+
+#Convolutional Layer 7
 net.conv7.weight.data = torch.from_numpy(np.transpose(weights[36]))
-# net.conv1[0].bias.data = torch.from_numpy(weights[37])
-# net.conv1[1].bias.data = torch.from_numpy(weights[38])
-# net.conv1[2].bias.data = torch.from_numpy(weights[39])
-# net.conv1[3].bias.data = torch.from_numpy(weights[40])
-# net.conv1[4].bias.data = torch.from_numpy(weights[41])
+net.conv7.bias.data = torch.from_numpy(weights[37])
+
+#Batch Layer 7
+net.batch7.weight.data = torch.from_numpy(weights[38])
+net.batch7.bias[0].data = torch.from_numpy(weights[39])
+net.batch7.bias[1].data = torch.from_numpy(weights[40])
+net.batch7.bias[2].data = torch.from_numpy(weights[41])
 
 net.spikes.weight.data = torch.from_numpy(np.transpose(weights[42]))
-# net.conv1[0].bias.data = torch.from_numpy(weights[43])
+net.conv1.bias.data = torch.from_numpy(weights[43])
 
 
 net.soma.weight.data = torch.from_numpy(np.transpose(weights[44]))
-# net.conv1[0].bias.data = torch.from_numpy(weights[45])
+net.conv1.bias.data = torch.from_numpy(weights[45])
 
 
 net.spikes.weight.data = torch.from_numpy(np.transpose(weights[46]))
-# net.conv1[0].bias.data = torch.from_numpy(weights[47])
+net.conv1.bias.data = torch.from_numpy(weights[47])
 
 
